dea6af5e8d66126b4383711a6b85a277d3eb0a35
VipulDeshwal <deshwalvipul132@gmail.com>
2026-01-12 15:08:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Vipul Deshwal", 
+    author_email="deshwalvipul132@gmail.com",
+    packages=find_packages(),
+
+
+
+)
